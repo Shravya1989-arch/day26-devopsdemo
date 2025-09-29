07ccd4a9f633c0ee74d265ccd1bbec63cc39a368
Shravya1989-arch <shravya1989@gmail.com>
2025-09-29 17:12:16 +0530

    Changes done to the devops files.

--- dev/backend.tf
+++ dev/backend.tf
@@ -3,7 +3,9 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfdevbackend25shra"
     container_name      = "tfstate"
-    key                 = "dev.tfstate"
+    key                 = "dev.terraform.tfstate"
+    subscription_id       = "287af259-8e7d-4ff7-b511-52679e90db97"
+    // use_azuread_auth      = true
   }
 }
 
--- stage/backend.tf
+++ stage/backend.tf
@@ -3,7 +3,9 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfstage25shra"
     container_name      = "tfstate"
-    key                 = "stage.tfstate"
+    key                 = "stage.terraform.tfstate"
+    subscription_id       = "287af259-8e7d-4ff7-b511-52679e90db97"
+    // use_azuread_auth      = true
   }
 }
 
